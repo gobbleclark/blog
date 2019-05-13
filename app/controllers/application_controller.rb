b4bc7d2376f5f5c55d2f45cedc3bc863adbfb4b8
Gavin Clark <gavingclark4@gmail.com>
2019-05-13 13:51:34 -0600

    yes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    def index
+        @pages = Page.all
+    end
+
+    def show
+        @page = page.find(params)
+    end
+
+    def new #this would be to add a link to add a new page/blog post
+
+
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def updated
+    end
+
+    def destroy
+    end
+
+
 end
